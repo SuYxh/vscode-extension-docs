9cfb8088d10bb0dcd0b6881c0809eae203283f6b
yangxinhao <yangxinhao@zhuanzhuan.com>
2023-08-15 10:19:58 +0800

    feat: 增加 nav

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,17 +1,14 @@
 import type { DefaultTheme } from "vitepress";
 
 export const nav: DefaultTheme.Config["nav"] = [
-  // {
-  //   text: "前端基础",
-  //   items: [
-  //     { text: "JavaScript 基础知识", link: "/fe/javascript/types" },
-  //     {
-  //       text: "CSS 相关",
-  //       items: [
-  //         { text: "CSS 语法", link: "/fe/css/spec" },
-  //       ],
-  //     },
-  //   ],
-  //   activeMatch: "^/fe",
-  // },
+  {
+    text: "使用文档",
+    link: "/use/",
+    activeMatch: "^/use",
+  },
+  {
+    text: "开发指南",
+    link: "/development/",
+    activeMatch: "^/development",
+  },
 ];
